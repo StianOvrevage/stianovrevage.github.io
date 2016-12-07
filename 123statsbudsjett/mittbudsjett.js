b174052f436881f54b745a1165ec97a52996c077
Stian Øvrevåge <sovrevage@gmail.com>
2016-12-07 14:29:20 +0700

    First commit of 123statsbudsjett

---
+++ 123statsbudsjett/mittbudsjett.js
@@ -0,0 +1,16 @@
+
+
+ // Source: https://www.ssb.no/arbeid-og-lonn/statistikker/lonnansatt/aar/2016-03-03
+var income = {
+  "average_lonnsinntekt_per_mnd": 44000,
+  "kapitalinntekt": 10000,
+  "snitt_gjeld": 1459900
+};
+
+// Source: https://www.regjeringen.no/no/dokumenter/prop.-1-ls-ls0-20162017/id2514108/sec2#KAP5-6
+var tax_rates = {
+  "skatt_kommune": 11.80,
+  "skatt_fylke": 2.65,
+  "skatt_staten": 9.55,
+  "trygdeavgift": 8.2
+};
